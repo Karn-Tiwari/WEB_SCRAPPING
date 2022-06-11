682cfdda7e9aa5d84a878630a2117b2cbde5cd61
Karn Tiwari <karntiwari5353@gmail.com>
2022-06-11 23:39:56 +0530

    ________

--- tinkerjson.js
+++ tinkerjson.js
@@ -1,6 +1,31 @@
 let fs = require("fs");
-let buffer = fs.readFileSync("./example.json");
-console.log(buffer);
-console.log("----------------------------");
-let data = JSON.parse(buffer);
-console.log(data);
+
+// let buffer = fs.readFileSync("./example.json");
+// console.log(buffer);
+// console.log("----------------------------");
+// //Array k form me hai
+
+// let data = JSON.parse(buffer);
+let data = require("./example.json");// ye direct require parse wala step hata karke seedha require karta hai 
+
+// console.log(data);
+// data.push({
+//     "name": "KARN",
+//     "last_name": "Jobs",
+//     "isAvenger": true,
+//     "friends": [
+//         "Bill",
+//         "Mark",
+//         "Steve"
+//     ],
+//     "age": 56,
+//     "address": {
+//         "city": "Cupertino",
+//         "state": "California"
+//     }
+
+// });
+// // ye data ko write karne k liye jisse kuch change kar sake
+
+// let stringData = JSON.stringify(data);
+// fs.writeFileSync("./example.json", stringData);
